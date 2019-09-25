9b7a1a37d795e87097be31d5cc0fcc65c453d709
Kuran V Kumar <kurankumar@gmail.com>
2019-09-25 19:20:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -31,6 +31,9 @@ def using_concat(array1, array2)
   array1.concat(array2)
   
   
-def using_insert(array)
-  array.insert()
+def using_insert(array, new_element)
+  array.insert(3, new_element)
+  
+def using_uniq
+  
   
